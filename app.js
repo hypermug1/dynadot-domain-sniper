20b304d5ed49392f564c98799bc56b682c5e3b66
hypermug1 <63418967+hypermug1@users.noreply.github.com>
2024-03-08 16:13:30 -0500

    Update app.js
    :trollface:

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 const axios = require('axios');
-const twilio = require("twilio");
-const xml2js = require('xml2js');
 const util = require('util');
 
-// Configuration for Dynadot
-const API_KEY = 'YOUR_DYNADOT_API_KEY'
-// Domain name to snipe e.g. ilovejazz.com
-const DOMAIN_NAME = 'YOUR_DOMAIN_NAME'
+// DYNADOT API SHIT 🔥
+const API_KEY = 'DYNADOT_API_KEY';
+const DOMAIN_NAME = 'DOMAIN_TO_SNIPE'
 
-// Configuration for Twilio
-const accountSid = "YOUR_TWILIO_ACCOUNT_SID";
-const authToken = "YOUR_TWILIO_AUTH_TOKEN";
-const client = new twilio(accountSid, authToken);
-
-// Function to send message
-const sendMessage = async (message) => {
-  await client.messages.create({
-    body: message,
-    from: "whatsapp:SENDER_WHATSAPP_NUMBER",
-    to: "whatsapp:RECEIVER_WHATSAPP_NUMBER",
-  });
+const DISCORD_ID = 'YOUR_DISCORD_ID'
+// DISCORD SHIT 🔥
+const discordWebhookUrl = 'DISCORD_WEBHOOK_URL;
+const DISCORD_ID = 'YOUR_DISCORD_ID'
+// DISCORD MESSAGE SENDER CONTRAPTION 🔥
+const sendDiscordMessage = async (content) => {
+  await axios.post(discordWebhookUrl, { content });
 };
 
-// Function to check and register domain
+// CAN WE REGISTER? 🔥
 const checkAndRegisterDomain = async () => {
   try {
     const searchResponse = await axios.get(`https://api.dynadot.com/api3.json?key=${API_KEY}&command=search&domain0=${DOMAIN_NAME}`);
@@ -31,34 +23,32 @@ const checkAndRegisterDomain = async () => {
     const searchResult = result.SearchResponse.SearchResults[0];
     
     if (searchResult.Available === 'no') {
-      console.log(`Domain ${DOMAIN_NAME} is not available.`);
+      console.log(`Domain ${DOMAIN_NAME} is not available :(`);
     } else {
       const registerResponse = await axios.get(`https://api.dynadot.com/api3.json?key=${API_KEY}&command=register&domain=${DOMAIN_NAME}&currency=EUR&duration=1`);
       const registerResult = registerResponse.data.RegisterResponse;
       
       if (registerResult.ResponseCode === '0' && registerResult.Status === 'success') {
         console.log(`Domain ${DOMAIN_NAME} registered successfully.`);
-        await sendMessage(`Domain ${DOMAIN_NAME} registered 🧿🧿🧿`)
+        await sendDiscordMessage(`Domain ${DOMAIN_NAME} registered :) <@${DISCORD_ID}>`);
         clearInterval(intervalId);
         process.exit(); 
       } else {
         console.log(`Failed to register domain ${DOMAIN_NAME}.`);
-        await sendMessage(`Domain ${DOMAIN_NAME} FAILURE`);
+        await sendDiscordMessage(`Domain ${DOMAIN_NAME} FAILURE`);
       }
     }
   } catch (error) {
     console.error(`An error occurred: ${error}`);
   }
 };
-
-// Function to notify server health
+// ARE WE UP?!?! 🔥
 const serverHealthNotification = async () => {
-    console.log('Server up and running');
-    sendMessage(`Server up and running, trying to snipe ${DOMAIN_NAME}`);
-}
+  console.log('Server up and running');
+};
 
-// Set interval to check and register domain every 10 seconds
-const intervalId = setInterval(checkAndRegisterDomain, 10 * 1000);
+// HUMP DYNADOT'S LEG EVERY 60 SECONDS 🔥
+const intervalId = setInterval(checkAndRegisterDomain, 60 * 1000);
 
-// Call server health notification function
+// CHECK IF WE UP 🔥
 serverHealthNotification();
